5a758ea3b90107fc740b1db4bbcb00600dd71a5d
Mah <madavetto@gmail.com>
2023-05-03 20:25:09 -0300

    update

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-let imagemDaEstrada;
-let imagemDoAtor;
 
 
-function preload() {
-  imagemDaEstrada = loadImage("imagens/estrada.png");
-  imagemDoAtor = loadImage("imagens/ator-1.png");
-}
+//carro
+let xCarro = 420; //600;
+
+//ator
+let yAtor = 366;
+
 
 function setup() {
   createCanvas(500, 400);
@@ -13,5 +13,30 @@ function setup() {
 
 function draw() {
   background(imagemDaEstrada);
-  image(imagemDoAtor, 100, 366);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+}
+
+function mostraAtor() {
+    image(imagemDoAtor, 100, yAtor, 30, 30);
+}
+
+function mostraCarro() {
+    image(imagemDoCarro, xCarro, 40, 50, 40);
+}
+
+function movimentaCarro() {
+   // xCarro = xCarro - 2; reescrevi abaixo, ficou mais enxuto :)
+    xCarro -= 2;
+}
+
+function movimentaAtor() {
+    if (keyIsDown(UP_ARROW)) {
+        yAtor -= 3;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+        yAtor += 3;
+    }
 }
