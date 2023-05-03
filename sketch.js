21b2f1d00a384a72a695121e1648a1b5a3a46306
Mah <madavetto@gmail.com>
2023-05-03 19:38:50 -0300

    update

---
+++ sketch.js
@@ -0,0 +1,17 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+
+
+function preload() {
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+}
+
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  image(imagemDoAtor, 100, 366);
+}
